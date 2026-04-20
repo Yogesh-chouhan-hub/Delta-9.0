10f8f32e104012ad4b4b114b0cd7dd70e6aac6e0
Yogesh-chouhan-hub <yogeshchouhan2253@gmail.com>
2026-04-20 20:33:46 +0530

    get and post request learned

--- REST/index.js
+++ REST/index.js
@@ -1,14 +1,17 @@
+const { error } = require("console");
 const express = require("express")
 const path = require("path");
+const {v4:uuidv4} = require("uuid")
+
 const app = express();
 const port = 8080;
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
-app.set("view engine","ejs");
 
+app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"))
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 //Public for css rendering 
 //while view for ejs templates
 //app.set("folder-name",path.join(__dirname,"folder-name"))
@@ -16,20 +19,24 @@ app.set(express.static(path.join(__dirname,"public")));
 
 let posts = [
     {
-        username : 'Yogesh chouhan',
-        content : 'i am iron man...'
+        id:uuidv4(),
+        username: 'Yogesh chouhan',
+        content: 'i am iron man...'
     },
     {
-        username : 'Jyoti chouhan',
-        content : 'Yohohohohohoho...'
+        id:uuidv4(),
+        username: 'Jyoti chouhan',
+        content: 'Yohohohohohoho...'
     },
     {
-        username : 'Tilak chouhan',
-        content : 'Hardest choices require strongest will...'
+        id:uuidv4(),
+        username: 'Tilak chouhan',
+        content: 'Hardest choices require strongest will...'
     },
     {
-        username : 'Renu chouhan',
-        content : 'Hardest choices has to be made for the success...'
+        id:uuidv4(),
+        username: 'Renu chouhan',
+        content: 'Hardest choices has to be made for the success...'
     }
 ]
 
@@ -38,6 +45,45 @@ app.get('/posts',(req,res)=>{
     res.render("index.ejs",{posts});
 })
 
+
+app.get('/post/new',(req,res)=>{
+    res.render("new.ejs");
+})
+
+app.post('/posts',(req,res)=>{
+    let {username,content} = req.body;
+    let id = uuidv4();
+    posts.push({id,username,content});
+    res.redirect("/posts"); // bydefault get request....
+})
+
+app.get('/posts/:id',(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=>id === p.id)
+    if (post){
+        res.render("show.ejs",{post});
+    }else{
+        res.send(`<h1>User does'nt exist....</h1>`)
+    }
+    //let post = posts.find((p)=>{
+    //return id === p.id;
+    //})
+})
+
+app.get('/posts/update/:id',(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=>id===p.id)
+    //res.send(post)
+    res.render('update.ejs',{post})
+})
+
+app.patch('/posts/update/:id',(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=>id === p.id);
+    post.content = newContent;
+    res.send("update successfully...");
+})
 
 app.listen(port,()=>{
     console.log("Server listening on port 8080");
